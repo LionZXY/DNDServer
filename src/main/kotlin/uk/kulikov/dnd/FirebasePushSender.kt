d0d7da6c8e452baa2a22fd56e64b1cdc8f54f7e9
Programistich <dzhos.oleksii@gmail.com>
2024-09-28 19:07:34 +0300

    Support setup dnd status

--- src/main/kotlin/uk/kulikov/dnd/FirebasePushSender.kt
+++ src/main/kotlin/uk/kulikov/dnd/FirebasePushSender.kt
@@ -4,20 +4,22 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.messaging.AndroidConfig
+import com.google.firebase.messaging.ApnsConfig
+import com.google.firebase.messaging.Aps
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.Message
 import com.google.firebase.messaging.Notification
 import java.io.FileInputStream
 
-fun sendPush(clientToken: String) {
-    val googleCredentials = GoogleCredentials
-        .fromStream(FileInputStream("private-key.json"))
-        .createScoped(listOf("https://www.googleapis.com/auth/firebase.messaging"));
-    val options = FirebaseOptions.builder()
-        .setCredentials(googleCredentials)
-        .build()
-    val firebaseApp = FirebaseApp.initializeApp(options)
+private val googleCredentials = GoogleCredentials
+    .fromStream(FileInputStream("private-key.json"))
+    .createScoped(listOf("https://www.googleapis.com/auth/firebase.messaging"));
+private val options = FirebaseOptions.builder()
+    .setCredentials(googleCredentials)
+    .build()
+private val firebaseApp = FirebaseApp.initializeApp(options)
 
+fun sendPush(clientToken: String) {
     val message: Message = Message.builder()
         .setAndroidConfig(
             AndroidConfig.builder()
@@ -26,6 +28,38 @@ fun sendPush(clientToken: String) {
         )
         .setNotification(Notification.builder().setTitle("Hello!").build())
         .setToken(clientToken)
+        .build()
+
+    val response = FirebaseMessaging.getInstance(firebaseApp).send(message)
+
+    println("Successfully sent message: $response")
+}
+
+
+fun sendDNDStatusPush(clientToken: String, dnd: Boolean) {
+    val message: Message = Message.builder()
+        .setAndroidConfig(
+            AndroidConfig.builder()
+                .setPriority(AndroidConfig.Priority.HIGH)
+                .build()
+        )
+        .setApnsConfig(
+            ApnsConfig.builder()
+                .setAps(
+                    Aps.builder()
+                        .setContentAvailable(true)  // For background notifications
+                        .build()
+                )
+                .build()
+        )
+        .setNotification(
+            Notification.builder()
+                .setTitle("Changed DND status")
+                .setBody("DND is now ${if (dnd) "on" else "off"}")
+                .build()
+        )
+        .setToken(clientToken)
+        .putData("dnd", dnd.toString())
         .build()
 
     val response = FirebaseMessaging.getInstance(firebaseApp).send(message)
